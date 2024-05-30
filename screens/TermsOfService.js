a2a85d94d171f1b06f019e04c997a014da944f11
윤영서 <dbsys1120@naver.com>
2024-05-30 16:26:58 +0900

    feat : 이용약관 동의 모달창

--- screens/TermsOfService.js
+++ screens/TermsOfService.js
@@ -1,11 +1,11 @@
 // 이용약관동의
 import React,{useState} from 'react';
-import { StyleSheet, Text, View, Image, TouchableOpacity,Linking, Switch} from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity,Linking, ScrollView} from 'react-native';
 import arrowToLeft from '../assets/images/arrowToLeft.png';
 import { useNavigation } from '@react-navigation/native';
 import { Button } from 'react-native-paper';
 import Toggle from '../components/toggle.js';
-
+import { useHTML } from 'react-native-html-render';
 
 export default function TermsOfService() {
     const navigation = useNavigation();
@@ -52,43 +52,61 @@ export default function TermsOfService() {
         {isModalVisible && (
             <View style={styles.modalContainer}>
             <View style={styles.modalContent}>
+            <ScrollView contentContainerStyle={{ padding: 22 }}>
                 <Text style={styles.modalText}>
-                **앱 이용약관 동의서**
+                <Text style={{fontWeight:600, color:'black', fontSize:20}}>앱 이용약관 동의서{"\n"}</Text>
+                {"\n"}
+                <Text style={{fontSize:10}}>이용자는 본 앱을 사용함으로써 아래의 이용약관에 동의하는 것으로 간주됩니다.{"\n"} 본 이용약관을 주의 깊게 읽어주시기 바랍니다.{"\n"}</Text>
+                {"\n"}
 
-                        이용자는 본 앱을 사용함으로써 아래의 이용약관에 동의하는 것으로 간주됩니다. 본 이용약관을 주의 깊게 읽어주시기 바랍니다.
+                <Text style={{fontWeight:400, color:'black', fontSize:15}}>1. 약관의 효력 및 변경{"\n"}</Text>
+                <Text style={{color:'black', fontSize:11, lineHeight:19}}>•   본 약관은 이용자가 본 앱을 다운로드하거나 설치하는 즉시 효력을 발생합니다.{"\n"}</Text>
+                <Text style={{color:'black', fontSize:11, lineHeight:19}}>•   회사는 필요 시 약관을 변경할 수 있으며, 변경된 약관은 앱 내에 공지된 후 효력이 발생합니다.{"\n"}</Text>
+                {"\n"}
+                
+                <Text style={{fontWeight:400, color:'black', fontSize:15}}>2. 서비스의 제공 및 변경{"\n"}</Text>
+                <Text style={{color:'black', fontSize:11, lineHeight:19}}>•   회사는 본 앱을 통해 다양한 서비스를 제공합니다.{"\n"}</Text>
+                <Text style={{color:'black', fontSize:11, lineHeight:19}}>•   회사는 서비스의 내용, 형태 등을 변경할 수 있으며, 변경 사항은 사전 공지합니다.{"\n"}</Text>
+                {"\n"}
 
-                        1. **약관의 효력 및 변경**
-                            - 본 약관은 이용자가 본 앱을 다운로드하거나 설치하는 즉시 효력을 발생합니다.
-                            - 회사는 필요 시 약관을 변경할 수 있으며, 변경된 약관은 앱 내에 공지된 후 효력이 발생합니다.
-                        2. **서비스의 제공 및 변경**
-                            - 회사는 본 앱을 통해 다양한 서비스를 제공합니다.
-                            - 회사는 서비스의 내용, 형태 등을 변경할 수 있으며, 변경 사항은 사전 공지합니다.
-                        3. **이용자의 의무**
-                            - 이용자는 법령 및 본 약관을 준수해야 합니다.
-                            - 이용자는 회사의 사전 승낙 없이 본 앱을 상업적으로 이용할 수 없습니다.
-                            - 이용자는 본 앱을 통해 얻은 정보를 회사의 사전 동의 없이 제3자에게 제공하거나 공유할 수 없습니다.
-                        4. **개인정보의 보호**
-                            - 회사는 이용자의 개인정보를 보호하기 위해 노력하며, 관련 법령에 따라 개인정보를 처리합니다.
-                            - 개인정보 수집 및 이용에 대한 자세한 내용은 개인정보처리방침을 참조하십시오.
-                        5. **서비스의 중단**
-                            - 회사는 다음과 같은 사유로 서비스 제공을 일시 중단할 수 있습니다:
-                                1. 시스템 점검 또는 유지보수
-                                2. 전기통신사업법에 따른 기간통신사업자가 전기통신 서비스를 중단한 경우
-                                3. 기타 불가항력적 사유
-                        6. **면책조항**
-                            - 회사는 이용자의 귀책사유로 인한 서비스 이용의 장애에 대하여 책임을 지지 않습니다.
-                            - 회사는 천재지변, 전쟁, 기타 불가항력으로 인해 서비스를 제공할 수 없는 경우 책임을 지지 않습니다.
-                        7. **준거법 및 관할법원**
-                            - 본 약관은 대한민국 법률에 따라 규율되고 해석됩니다.
-                            - 본 약관과 관련된 분쟁은 회사의 본사 소재지를 관할하는 법원의 전속 관할로 합니다.
+                <Text style={{fontWeight:400, color:'black', fontSize:15}}>3. 이용자의 의무{"\n"}</Text>
+                <Text style={{color:'black', fontSize:11, lineHeight:19}}>•   이용자는 법령 및 본 약관을 준수해야 합니다.{"\n"}</Text>
+                <Text style={{color:'black', fontSize:11, lineHeight:19}}>•   이용자는 회사의 사전 승낙 없이 본 앱을 상업적으로 이용할 수 없습니다.{"\n"}</Text>
+                <Text style={{color:'black', fontSize:11, lineHeight:19}}>•   이용자는 본 앱을 통해 얻은 정보를 회사의 사전 동의 없이 제3자에게 제공하거나 공유할 수 없습니다.{"\n"}</Text>
+                {"\n"}
 
-                        이용자는 본 약관에 동의하며, 이에 따라 서비스를 이용할 것을 확인합니다.
+                <Text style={{fontWeight:400, color:'black', fontSize:15}}>4. 개인정보의 보호{"\n"}</Text>
+                <Text style={{color:'black', fontSize:11, lineHeight:19}}>•   회사는 이용자의 개인정보를 보호하기 위해 노력하며, 관련 법령에 따라 개인정보를 처리합니다.{"\n"}</Text>
+                <Text style={{color:'black', fontSize:11, lineHeight:19}}>•   개인정보 수집 및 이용에 대한 자세한 내용은 개인정보처리방침을 참조하십시오.{"\n"}</Text>
+                {"\n"}
+
+                <Text style={{fontWeight:400, color:'black', fontSize:15}}>5. 서비스의 중단{"\n"}</Text>
+                <Text style={{color:'black', fontSize:11, lineHeight:19}}>•   회사는 다음과 같은 사유로 서비스 제공을 일시 중단할 수 있습니다:{"\n"}</Text>
+                <Text style={{color:'black', fontSize:11, lineHeight:19}}>    1 . 시스템 점검 또는 유지보수{"\n"}</Text>
+                <Text style={{color:'black', fontSize:11, lineHeight:19}}>    2 . 전기통신사업법에 따른 기간통신사업자가 전기통신 서비스를 중단한 경우{"\n"}</Text>
+                <Text style={{color:'black', fontSize:11, lineHeight:19}}>    3 . 기타 불가항력적 사유{"\n"}</Text>
+                {"\n"}
+
+                <Text style={{fontWeight:400, color:'black', fontSize:15}}>6. 면책조항{"\n"}</Text>
+                <Text style={{color:'black', fontSize:11, lineHeight:19}}>•   회사는 이용자의 귀책사유로 인한 서비스 이용의 장애에 대하여 책임을 지지 않습니다.{"\n"}</Text>
+                <Text style={{color:'black', fontSize:11, lineHeight:19}}>•   회사는 천재지변, 전쟁, 기타 불가항력으로 인해 서비스를 제공할 수 없는 경우 책임을 지지 않습니다.{"\n"}</Text>
+                {"\n"}
+
+                <Text style={{fontWeight:400, color:'black', fontSize:15}}>7. 준거법 및 관할법원{"\n"}</Text>
+                <Text style={{color:'black', fontSize:11, lineHeight:19}}>•   본 약관은 대한민국 법률에 따라 규율되고 해석됩니다.{"\n"}</Text>
+                <Text style={{color:'black', fontSize:11, lineHeight:19}}>•   본 약관과 관련된 분쟁은 회사의 본사 소재지를 관할하는 법원의 전속 관할로 합니다.{"\n"}</Text>
+                {"\n"}
+                <Text style={{fontSize:12, color:'black'}}>이용자는 본 약관에 동의하며, 이에 따라 서비스를 이용할 것을 확인합니다.{"\n"}</Text>
+
+
                 </Text>
                 <View style={styles.modalButtonContainer}>
                 <Button mode="outlined" onPress={handleCancelModal} style={styles.modalButton}>
                     닫기
                 </Button>
                 </View>
+                
+                </ScrollView>
             </View>
             </View>
             )}
@@ -96,54 +114,73 @@ export default function TermsOfService() {
         {isModalVisible2 && (
                     <View style={styles.modalContainer}>
                     <View style={styles.modalContent}>
+                    <ScrollView contentContainerStyle={{ padding: 22 }}>
                         <Text style={styles.modalText}>
-                        **개인정보 보호 동의서**
+                        <Text style={{fontWeight:600, color:'black', fontSize:20}}>개인정보 보호 동의서{"\n"}</Text>
+                        {"\n"}
+                        <Text style={{fontSize:10}}>이용자는 본 앱을 사용함으로써 아래의 개인정보 보호 동의서에 동의하는 것으로 간주됩니다.{"\n"}본 동의서를 주의 깊게 읽어주시기 바랍니다.</Text>
+                        {"\n"}
 
-                        이용자는 본 앱을 사용함으로써 아래의 개인정보 보호 동의서에 동의하는 것으로 간주됩니다. 본 동의서를 주의 깊게 읽어주시기 바랍니다.
+                        <Text style={{fontWeight:400, color:'black', fontSize:15}}>1. 개인정보의 수집 및 이용 목적{"\n"}</Text>
+                        <Text style={{color:'black', fontSize:11, lineHeight:19}}>•   본 앱은 다음과 같은 목적을 위해 이용자의 개인정보를 수집 및 이용합니다:{"\n"}</Text>
+                        <Text style={{color:'black', fontSize:11, lineHeight:19}}>    1 . 서비스 제공 및 운영{"\n"}</Text>
+                        <Text style={{color:'black', fontSize:11, lineHeight:19}}>    2 . 고객 지원 및 문의 대응{"\n"}</Text>
+                        <Text style={{color:'black', fontSize:11, lineHeight:19}}>    3 . 마케팅 및 광고{"\n"}</Text>
+                        <Text style={{color:'black', fontSize:11, lineHeight:19}}>    4 . 서비스 개선 및 연구 분석{"\n"}</Text>
+                        {"\n"}
+                        
+                        <Text style={{fontWeight:400, color:'black', fontSize:15}}>2. 수집하는 개인정보의 항목{"\n"}</Text>
+                        <Text style={{color:'black', fontSize:11, lineHeight:19}}>•   본 앱은 다음과 같은 개인정보를 수집합니다:{"\n"}</Text>
+                        <Text style={{color:'black', fontSize:11, lineHeight:19}}>    1 . 필수 정보: 이름, 나이, 성별, 지역{"\n"}</Text>
+                        <Text style={{color:'black', fontSize:11, lineHeight:19}}>    2 . 선택 정보: 가게이름, 가게연락처, 가게주소, 가게이미지{"\n"}</Text>
+                        {"\n"}
 
-                        1. **개인정보의 수집 및 이용 목적**
-                            - 본 앱은 다음과 같은 목적을 위해 이용자의 개인정보를 수집 및 이용합니다:
-                                1. 서비스 제공 및 운영
-                                2. 고객 지원 및 문의 대응
-                                3. 마케팅 및 광고
-                                4. 서비스 개선 및 연구 분석
-                        2. **수집하는 개인정보의 항목**
-                            - 본 앱은 다음과 같은 개인정보를 수집합니다:
-                                1. 필수 정보: 이름, 나이, 성별, 지역
-                                2. 선택 정보: 가게이름, 가게연락처, 가게주소, 가게이미지
-                        3. **개인정보의 보유 및 이용 기간**
-                            - 이용자의 개인정보는 수집 및 이용 목적이 달성될 때까지 보유 및 이용됩니다.
-                            - 관련 법령에 따라 보존할 필요가 있는 경우, 해당 법령에서 정한 기간 동안 보관합니다.
-                        4. **개인정보의 제3자 제공**
-                            - 본 앱은 원칙적으로 이용자의 개인정보를 제3자에게 제공하지 않습니다.
-                            - 다만, 다음의 경우에는 예외로 합니다:
-                                1. 이용자가 사전에 동의한 경우
-                                2. 법령에 따라 요구되는 경우
-                                3. 서비스 제공을 위해 필요한 경우(예: 배송 업체)
-                        5. **개인정보의 보호 조치**
-                            - 회사는 이용자의 개인정보를 안전하게 보호하기 위해 다음과 같은 조치를 취합니다:
-                                1. 개인정보의 암호화
-                                2. 접근 통제 및 관리
-                                3. 개인정보 보호 교육
-                        6. **이용자의 권리**
-                            - 이용자는 언제든지 자신의 개인정보를 열람, 수정, 삭제할 수 있습니다.
-                            - 개인정보 열람과 수정은 앱 내에서, 삭제 요청은 고객 지원을 통해 가능합니다.
-                            - 이용자는 개인정보 처리에 대한 동의를 철회할 수 있으며, 철회 시 서비스 이용이 제한될 수 있습니다.
-                        7. **개인정보 보호 책임자**
-                            - 개인정보 보호 책임자: 문채원
-                            - 연락처: 010-8841-2594
-                            - 이메일: chae.won2594@gmail.com
+                        <Text style={{fontWeight:400, color:'black', fontSize:15}}>3. 개인정보의 보유 및 이용 기간{"\n"}</Text>
+                        <Text style={{color:'black', fontSize:11, lineHeight:19}}>•   이용자의 개인정보는 수집 및 이용 목적이 달성될 때까지 보유 및 이용됩니다.{"\n"}</Text>
+                        <Text style={{color:'black', fontSize:11, lineHeight:19}}>•   관련 법령에 따라 보존할 필요가 있는 경우, 해당 법령에서 정한 기간 동안 보관합니다.{"\n"}</Text>
+                        {"\n"}
 
-                        이용자는 본 동의서의 내용을 충분히 이해하였으며, 이에 동의합니다.
+                        <Text style={{fontWeight:400, color:'black', fontSize:15}}>4. 개인정보의 제3자 제공{"\n"}</Text>
+                        <Text style={{color:'black', fontSize:11, lineHeight:19}}>•   본 앱은 원칙적으로 이용자의 개인정보를 제3자에게 제공하지 않습니다.{"\n"}</Text>
+                        <Text style={{color:'black', fontSize:11, lineHeight:19}}>•   다만, 다음의 경우에는 예외로 합니다:{"\n"}</Text>
+                        <Text style={{color:'black', fontSize:11, lineHeight:19}}>    1 . 이용자가 사전에 동의한 경우{"\n"}</Text>
+                        <Text style={{color:'black', fontSize:11, lineHeight:19}}>    2 . 법령에 따라 요구되는 경우{"\n"}</Text>
+                        <Text style={{color:'black', fontSize:11, lineHeight:19}}>    3 . 서비스 제공을 위해 필요한 경우(예: 배송 업체){"\n"}</Text>
+                        {"\n"}
+
+                        <Text style={{fontWeight:400, color:'black', fontSize:15}}>5. 개인정보의 보호 조치{"\n"}</Text>
+                        <Text style={{color:'black', fontSize:11, lineHeight:19}}>•   회사는 이용자의 개인정보를 안전하게 보호하기 위해 다음과 같은 조치를 취합니다:{"\n"}</Text>
+                        <Text style={{color:'black', fontSize:11, lineHeight:19}}>    1 . 개인정보의 암호화{"\n"}</Text>
+                        <Text style={{color:'black', fontSize:11, lineHeight:19}}>    2 . 접근 통제 및 관리{"\n"}</Text>
+                        <Text style={{color:'black', fontSize:11, lineHeight:19}}>    3 . 개인정보 보호 교육{"\n"}</Text>
+                        {"\n"}
+
+                        <Text style={{fontWeight:400, color:'black', fontSize:15}}>6. 이용자의 권리{"\n"}</Text>
+                        <Text style={{color:'black', fontSize:11, lineHeight:19}}>•   이용자는 언제든지 자신의 개인정보를 열람, 수정, 삭제할 수 있습니다.{"\n"}</Text>
+                        <Text style={{color:'black', fontSize:11, lineHeight:19}}>•   개인정보 열람과 수정은 앱 내에서, 삭제 요청은 고객 지원을 통해 가능합니다.{"\n"}</Text>
+                        <Text style={{color:'black', fontSize:11, lineHeight:19}}>•   이용자는 개인정보 처리에 대한 동의를 철회할 수 있으며, 철회 시 서비스 이용이 제한될 수 있습니다.{"\n"}</Text>
+                        {"\n"}
+
+                        <Text style={{fontWeight:400, color:'black', fontSize:15}}>7. 개인정보 보호 책임자{"\n"}</Text>
+                        <Text style={{color:'black', fontSize:11, lineHeight:19}}>•   개인정보 보호 책임자: 문채원{"\n"}</Text>
+                        <Text style={{color:'black', fontSize:11, lineHeight:19}}>•   연락처: 010-8841-2594{"\n"}</Text>
+                        <Text style={{color:'black', fontSize:11, lineHeight:19}}>•   이메일: chae.won2594@gmail.com{"\n"}</Text>
+                        {"\n"}
+                        <Text style={{fontSize:12, color:'black'}}>이용자는 본 동의서의 내용을 충분히 이해하였으며, 이에 동의합니다.{"\n"}</Text>
+
+
                         </Text>
                         <View style={styles.modalButtonContainer}>
                         <Button mode="outlined" onPress={handleCancelPrivacyModal} style={styles.modalButton}>
                             닫기
                         </Button>
                         </View>
+                        
+                        </ScrollView>
                     </View>
                     </View>
                     )}
+
         <View style={styles.toogleContainer}>
             <Text style={{fontSize: 15, color: '#000', marginRight: 20,fontFamily:'BlackHanSans-Regular',fontWeight:'800'}}> 알림 </Text>
             
@@ -227,6 +264,7 @@ const styles = StyleSheet.create({
     },
     modalContent: {
         width:'94%',
+        height:'80%',
         backgroundColor: 'white',
         padding: 22,
         borderRadius: 4,
@@ -251,5 +289,22 @@ const styles = StyleSheet.create({
     modalButton :{
         borderRadius:6,
         marginTop:8,
+    },
+    modalTextParagraph: {
+        fontSize: 16,
+        lineHeight: 24,
+        color: '#333',
+        marginBottom: 10,
+    },
+    modalTextSubheading: {
+    fontWeight: 'bold',
+    fontSize: 18,
+    lineHeight: 26,
+    marginBottom: 5,
+    },
+    modalTextList: {
+    fontSize: 16,
+    lineHeight: 24,
+    marginLeft: 20,
     },
 });
