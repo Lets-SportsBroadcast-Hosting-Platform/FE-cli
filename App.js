5b2316568d9bca54f47f00533d0b9d3cfe10ecdd
윤영서 <dbsys1120@naver.com>
2024-06-19 22:26:36 +0900

    feat : 가게 정보 수정 MyPlaceEdit에서 Toast추가

--- App.js
+++ App.js
@@ -35,7 +35,7 @@ export default function App() {
   return (
     <AuthProvider>
       <NavigationContainer>
-        <Stack.Navigator initialRouteName="Home">
+        <Stack.Navigator initialRouteName="MyHome">
           
           <Stack.Screen name="Home" component={Home} options={{headerShown:false}}/>
           
